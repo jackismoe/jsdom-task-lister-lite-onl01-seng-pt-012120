87759f5f3188672f993d4c58891d06d7410ae457
jackismoe <jacksonmo@me.com>
2020-08-29 12:37:38 -0400

    Resubmit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  const newTask = document.getElementById('create-task-form')
+  newTask = document.addEventListener('submit', createNewTask)
+})
+
+const createNewTask = (event) => {
+  event.preventDefault()
+  const newTaskDescription = document.getElementById('new-task-description')
+  const newTask = document.createElement('li')
+  newTask.innerText = newTaskDescription.value
+
+  appendNewTask(newTask)
+  event.target.reset()
+}
+
+const appendNewTask = (task) => {
+  document.getElementById('tasks').appendChild(task)
+}
