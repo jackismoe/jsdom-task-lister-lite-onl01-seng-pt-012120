aab6ad76787a3dfc266f7eb903f35c017f2c2601
jackismoe <jacksonmo@me.com>
2020-08-29 14:38:00 -0400

    Working on table creation

--- src/index.js
+++ src/index.js
@@ -1,18 +1,54 @@
+const taskTable = document.createElement('table')
+const tableHeaders = document.createElement('tr')
+tableHeaders.id = 'headers'
+const headerRow = document.getElementById('headers')
+headerRow.innerHTML = '<th id="title>Title</th><th id="description">Description"</th>'
+
+// tableHeaderTitle.innerText = 'Title'
+// tableHeaderDescription.innerText = 'Description'
+// taskTable.appendChild(tableHeaders)
+
 document.addEventListener('DOMContentLoaded', () => {
-  const newTask = document.getElementById('create-task-form')
-  newTask.addEventListener('submit', createNewTask)
+  const newTaskForm = document.getElementById('create-task-form')
+  newTaskForm.addEventListener('submit', createNewTask)
 })
 
 const createNewTask = (event) => {
   event.preventDefault()
-  const newTaskDescription = document.getElementById('new-task-description')
-  const newTask = document.createElement('li')
-  newTask.innerText = newTaskDescription.value
 
-  appendNewTask(newTask)
+// grab main
+  const mainContent = document.getElementById('main-content')
+
+// grab title
+  const newTaskTitle = document.querySelector('body div form input#task-title')
+
+// grab description
+  const newTaskDescription = document.querySelector('body div form input#task-description')
+
+// create table rows
+  const newRow = document.createElement('tr')
+  newRow.innerHTML = '<td id="task-title"></td><td id="task-description"></td>'
+
+// create table data
+
+  mainContent.appendChild(taskTable)
+  taskTable.appendChild(tableHeaderTitle)
+  taskTable.appendChild(tableHeaderDescription)
+  taskTable.appendChild(newRow)
+  
+  // titleData.innerText = newTaskTitle.value
+  // descriptionData.innerText = newTaskDescription.value
+
+  
+  // newTask.title.innerText = newTaskTitle.value
+  // newTask.description.innerText = newTaskDescription.value
+
+  // appendNewTask(newTask)
   event.target.reset()
 }
 
 const appendNewTask = (task) => {
   document.getElementById('tasks').appendChild(task)
 }
+
+
